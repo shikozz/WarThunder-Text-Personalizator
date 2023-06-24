78236afaad8486627851e46645153b22dab4a010
ShikoZZ <alexchanel12@gmail.com>
2023-06-25 01:02:21 +0300

    Create App

---
+++ WTTextPersonalizator/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using MessageBox = System.Windows.MessageBox;
+
+namespace WTTextPersonalizator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public string selectedFolder = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                label.Content = folderDlg.SelectedPath;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string path = label.Content.ToString() + "/lang/menu.csv";
+                string testStr = "";
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    testStr = reader.ReadToEnd();
+                }
+                WorkingFrame wf = new WorkingFrame(label.Content.ToString());
+                wf.Show();
+                this.Close();
+            }
+            catch 
+            {
+                MessageBox.Show("Укажиет корректный путь игры");
+            }
+            
+        }
+    }
+}
---
+++ WTTextPersonalizator/WorkingFrame.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WTTextPersonalizator
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkingFrame.xaml
+    /// </summary>
+    public partial class WorkingFrame : Window
+    {
+        string menuPath = "";
+        string menuString = "";
+        public WorkingFrame(string path)
+        {
+            InitializeComponent();
+            pathLabel.Content = path;
+            menuPath = path + "/lang/menu.csv";
+            readFiles();
+        }
+
+        public void readFiles()
+        {
+            using (StreamReader reader = new StreamReader(menuPath))
+            {
+                menuString = reader.ReadToEnd();
+                Console.WriteLine(menuString);
+            }
+        }
+
+        private void change1_Click(object sender, RoutedEventArgs e)
+        {
+            string goChange =text1.Text;
+            string res = "";
+            string init = init1.Text;
+            int index = menuString.IndexOf(init);
+            if (index >= 0)
+            {
+                res = menuString.Remove(index, init.Length).Insert(index, goChange);
+                File.WriteAllText(menuPath, res);
+                MessageBox.Show("Изменено успешно!");
+                init1.Text = "";
+                text1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Не найдено!");
+            }
+        }
+    }
+}
